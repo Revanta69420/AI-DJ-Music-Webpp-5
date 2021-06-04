1b78ebb53184d565b80a2fed7c235f687815d275
Revanta69420 <81847909+Revanta69420@users.noreply.github.com>
2021-06-04 18:30:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+song = "";
+song2 = "";
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function preload() {
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3")
+}
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    function modelLoaded() {
+        console.log('poseNet is initialized');
+    }
+    function gotPoses(results) {
+        if (results.length > 0) {
+            console.log(results);
+            scoreRightWrist = results[0].pose.keypoints[10].score;
+            scoreLeftWrist = results[0].pose.keypoints[9].score;
+            console.log("scoreLeftWrist = " + scoreLeftWrist);
+            leftWristX = results[0].pose.leftWrist.x;
+            leftWristY = results[0].pose.leftWrist.y;
+            console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+            rightWristX = results[0].pose.rightWrist.x;
+            rightWristY = results[0].pose.rightWrist.y;
+            console.log("rightWristX = " + rightWristX + "rightWristY" + rightWristY);
+        }
+    }
+}
+function draw() {
+    image(video, 0, 0, 600, 500);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+    }
+    if (scoreLeftWrist > 0.2) {
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimal = floor(InNumberleftWristY);
+        volume = remove_decimal / 500
+        document.getElementById("volume").innerHTML = "volume =  " + volume;
+        song.setVolume(volume);
+    }
+}
+function play() {
+    song.play();
+    song2.play();
+    song.setVolume(1);
+    song.rate(1);
+    song_leftWrist.isPlaying(song);
+    song_rightWrist.isPlaying(song2);
+}
